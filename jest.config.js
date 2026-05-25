eb8093a2fdd6cf05c0a07f398a0be577ba0cbfda
Nihith <nihithsistla98@gmail.com>
2026-05-26 01:17:20 +0530

    config: add Next.js project configuration and dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,49 +1,19 @@
-module.exports = {
-  testEnvironment: 'node',
+﻿const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '^@/types/(.*)$': '<rootDir>/types/$1',
-    '^@/api/(.*)$': '<rootDir>/api/$1',
-    '^@/stores/(.*)$': '<rootDir>/stores/$1',
-    '^@/queries/(.*)$': '<rootDir>/queries/$1',
-    '^@/(.*)$': '<rootDir>/$1'
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  transform: {
-    '^.+\\.(ts|tsx)$': ['babel-jest', {
-      presets: ['@babel/preset-env', '@babel/preset-typescript']
-    }],
-    '^.+\\.(js|jsx)$': 'babel-jest'
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|expo|@react-native|@babel|@testing-library|react-native-unistyles)/)'
-  ],
   testMatch: [
-    '**/tests/**/*.test.ts',
-    '**/tests/**/*.test.tsx',
-    '**/__tests__/**/*.ts',
-    '**/__tests__/**/*.tsx'
+    '**/__tests__/**/*.test.ts',
+    '**/__tests__/**/*.test.tsx',
   ],
-  collectCoverageFrom: [
-    'types/**/*.{ts,tsx}',
-    'api/**/*.{ts,tsx}',
-    'stores/**/*.{ts,tsx}',
-    'queries/**/*.{ts,tsx}',
-    'app/**/*.{ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-    '!**/.expo/**'
-  ],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/.expo/',
-    '/dist/'
-  ],
-  globals: {
-    'ts-jest': {
-      tsconfig: {
-        jsx: 'react'
-      }
-    }
-  }
-};
+}
+
+module.exports = createJestConfig(customJestConfig)
